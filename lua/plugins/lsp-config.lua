2b7cdc392b9e0f2f7e74297d0deaa4d36c070e67
David <davidgonzalez1019@gmail.com>
2026-06-28 16:39:42 -0500

    Added plugins

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,28 +2,29 @@ return {
 	{
 		"mason-org/mason.nvim",
 		opts = {},
-		config = function()
-			require("mason").setup()
-		end,
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
 		opts = {
 			auto_install = true,
 		},
 	},
 	{
 		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.lsp.config("lua_ls", { capabilities = capabilities })
-			vim.lsp.enable({ "lua_ls", "hyprls", "qmlls", "pyright" })
+		dependencies = { "saghen/blink.cmp" },
+		opts = {
+			servers = {
+				lua_ls = {},
+			},
+		},
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			-- local lspconfig = require("lspconfig")
-			-- lspconfig.tsserver.setup({ capabilities = capabilities })
-			-- lspconfig.html.setup({ capabilities = capabilities })
-			-- lspconfig.lua_ls.setup({ capabilities = capabilities })
 		end,
 	},
 }
